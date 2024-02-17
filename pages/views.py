ef6fc85e0ff8362183cd6454adbf4cbc049b054c
jvillada1 <jvilladac12@gmail.com>
2024-02-17 15:58:20 -0500

    se sube el segundo taller

--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,13 @@
 ##from django.shortcuts import render # here by default
 ##from django.http import HttpResponse # new
-from django.views.generic import TemplateView 
+from django.views.generic import TemplateView,ListView 
 from django.views import View 
 from django.shortcuts import render 
 from django.http import HttpResponse, HttpResponseRedirect 
 from django.urls import reverse 
 from django import forms
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404 
+from .models import Product
 
 
 # Create your views here.
@@ -49,13 +50,6 @@ class ContactPageView(TemplateView):
 
 
 
-class Product:
-    products = [
-        {"id":"1", "name":"TV", "description":"Best TV","price":"50"},
-        {"id":"2", "name":"iPhone", "description":"Best iPhone","price":"6000"},
-        {"id":"3", "name":"Chromecast", "description":"Best Chromecast","price":"4000"},
-        {"id":"4", "name":"Glasses", "description":"Best Glasses","price":"20"}
-    ]
 
 class ProductIndexView(View):
     template_name = 'products/index.html'
@@ -64,31 +58,59 @@ class ProductIndexView(View):
         viewData = {}
         viewData["title"] = "Products - Online Store"
         viewData["subtitle"] =  "List of products"
-        viewData["products"] = Product.products
+        viewData["products"] = Product.objects.all()
 
         return render(request, self.template_name, viewData)
 
 class ProductShowView(View):
     template_name = 'products/show.html'
 
-    def get(self, request, id):
-        viewData = {}
-        try:
-            product = Product.products[int(id) - 1]
-        except IndexError:
-            # Product number is not valid, redirect to home page
-            return HttpResponseRedirect(reverse('home'))
 
-        viewData["title"] = product["name"] + " - Online Store"
-        viewData["subtitle"] = product["name"] + " - Product information"
+    def get(self, request, id):
+
+        # Check if product id is valid
+        try:
+            product_id = int(id)
+            if product_id < 1:
+                raise ValueError("Product id must be 1 or greater")
+            product = get_object_or_404(Product, pk=product_id)
+        except (ValueError, IndexError):
+            # If the product id is not valid, redirect to the home page
+            return HttpResponseRedirect(reverse('home'))
+        
+        viewData = {}
+        product = get_object_or_404(Product, pk=product_id)
+        viewData["title"] = product.name + " - Online Store"
+        viewData["subtitle"] =  product.name + " - Product information"
         viewData["product"] = product
 
-        return render(request, self.template_name, viewData) 
+        return render(request, self.template_name, viewData)
 
 
-class ProductForm(forms.Form):
-    name = forms.CharField(required=True)
-    price = forms.FloatField(required=True) 
+class ProductListView(ListView):
+    model = Product
+    template_name = 'product_list.html'
+    context_object_name = 'products'  # This will allow you to loop through 'products' in your template
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Products - Online Store'
+        context['subtitle'] = 'List of products'
+        return context   
+
+
+
+
+
+
+
+
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['name', 'price']
+
     def clean_price(self):
         price = self.cleaned_data['price']
         if price <= 0:
@@ -108,7 +130,8 @@ class ProductCreateView(View):
 
     def post(self, request):
         form = ProductForm(request.POST)
-        if form.is_valid():
+        if form.is_valid(): 
+            form.save()
             # Save the product or perform any necessary actions
             # For demonstration, we're just redirecting to a success page
             return render(request, 'products/product_created.html')
